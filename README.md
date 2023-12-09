7e434e4a8f0df946be8b6b9ce707589e2e6a2f03
enonglosker <enonglosker@gmail.com>
2023-12-09 13:57:24 +0700

    add-menu book

---
+++ README.md
@@ -0,0 +1,8 @@
+<div class="" align="center">
+    <p>Api Menu Book</p>
+    <h1>API MENU BOOK</h1>
+    <span>✨⭐⭐⭐✨</span>
+    <hr>
+    <img src="Screenshot.png"/>
+    <br>
+</div>
--- buku_menu/settings.py
+++ buku_menu/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'img/'
+MEDIA_URL = ''
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
